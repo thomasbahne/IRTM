d182b7f71a1e2006cf961e374355d791bdb872a4
thomasbahne <thomas.bahne@gmx.net>
2021-05-05 17:03:40 +0200

    Added functions.py

---
+++ functions.py
@@ -0,0 +1,58 @@
+import matplotlib.pyplot
+
+# makes labels not run off the bottom of the graphic
+from matplotlib import rcParams
+
+import string
+
+# cleaning up the data:
+# for the purpose of my analysis (finding out if a recipe is vegan/vegetarian/omnivore, I can disregard many
+# descriptions of ingredients. Examples would be "fresh" or "mix", like in "fresh parsley", or "pudding mix".
+
+# What I did to the data for the ingredients_one_list:
+# Took "ingredients" column and fused the individual entries to a huge list - ingredients_one_list.csv
+
+data_folder_path = 'Data/'
+filename_recipes = 'RAW_recipes.csv'
+filename_ingredients = 'RAW_ingredients.csv'
+filename_ingredients_one_list = 'ingredients_one_list.csv'
+
+
+def plot_value_counts(data, upper_bound: int = None, lower_bound: int = None):
+    # Sighting the data: retrieve counts of ingredients
+    value_counts = data.value_counts(sort=True)
+    rcParams.update({'figure.autolayout': True})  # makes labels not run off the bottom of the graphic
+    if (upper_bound is None) & (lower_bound is None):
+        value_counts.plot(kind='barh')
+        matplotlib.pyplot.show()
+        return
+    elif upper_bound is None:
+        mask = value_counts > lower_bound
+    elif lower_bound is None:
+        mask = value_counts < upper_bound
+    else:
+        mask = (value_counts < upper_bound) & (value_counts > lower_bound)
+    value_counts = value_counts.loc[mask]
+    rcParams.update({'figure.autolayout': True})  # makes labels not run off the bottom of the graphic
+    value_counts.plot(kind='barh')
+    matplotlib.pyplot.show()
+
+
+def find_occurrence(data, word: string):
+    temp = set()
+    for item in data:
+        for ingredient in item:
+            if word in ingredient:
+                temp.add(ingredient)
+    return temp
+
+
+# only load useful columns with df = pd.read_csv("filepath", usecols=list_useful_column_names)
+# specify data types to take less memory (e.g. for year-numbers use int.16 instead of int.64)
+# command: df = pd.read_csv("train.csv", dtype={"column_name": "more_efficient_datatype"})
+# "categorical" is a datatype that uses minimal space if the entries in a column only take on
+# a few different values dtype={'column_name': 'category'}
+# replace missing values when loading data with 1) defining a convert function, e.g.
+# def convert(var): if var==np.nan: return "sth you want to have instead, or 0", return val
+# pd.read_csv('filepath', converters={'column_name': convert_function})
+# for test purposes, only load in a small part of the data with nrows=1000
