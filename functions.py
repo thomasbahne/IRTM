b29634041bbe23a4f51f16057856a68fd7e8f5e2
thomasbahne <thomas.bahne@gmx.net>
2021-05-09 22:37:35 +0200

    Began to implement new functions for REST API request

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot
+import pandas as pd
+import requests
 
 # makes labels not run off the bottom of the graphic
 from matplotlib import rcParams
@@ -19,7 +21,10 @@ filename_ingredients_one_list = 'ingredients_one_list.csv'
 
 
 def plot_value_counts(data, upper_bound: int = None, lower_bound: int = None):
-    # Sighting the data: retrieve counts of ingredients
+    # plots and return value counts for elements (ingredients) in data
+    # lower and upper bounds put restrictions on which value counts are shown
+    # specifying lower_bound => only ingredients with value counts > lower_bound are shown
+    # specifying upper_bound => only ingredients with value counts < upper_bound are shown
     work_data = None
     if isinstance(data[0], list):
         work_data = data.explode()
@@ -43,6 +48,7 @@ def plot_value_counts(data, upper_bound: int = None, lower_bound: int = None):
 
 
 def find_occurrence(data, word: string):
+    # finds all elements (ingredients) containing word in data
     temp = set()
     for item in data:
         for ingredient in item:
@@ -51,7 +57,83 @@ def find_occurrence(data, word: string):
     return temp
 
 
+def tokenize(phrase: str):
+    # tokenizes ingredients:
+    # removes (,), (*) and round brackets, replaces (&) with (and), strips trailing commas, deletes numbers
+    tokens = list(filter(lambda a: a != (',' or '*' or '(' or ')'), phrase.lower().split()))
+    tokens = ['and' if token=='&' else token for token in tokens]
+    tokens = [token.rstrip(',') for token in tokens]
+    tokens = [token for token in tokens if not token.isdigit()]
+    return tokens
 
+
+def remove_measure_units_single_recipe(ingredients: list, reference_units: pd.series.Series):
+    # removes all "measure units" and some other unnecessary descriptions specified in the reference list from
+    # a single list of ingredients/recipe
+    # reference list in stored in a .csv file (one row of strings)
+    cleaned_ingredients = list(map(tokenize, ingredients))
+    for token in cleaned_ingredients:
+        if token in reference_units:
+            cleaned_ingredients.remove(token)
+    return cleaned_ingredients
+
+
+def remove_measure_units(path_reference_units: str, data: pd.dataFrame.DataFrame):
+    # removes all "measure units" and some other unnecessary descriptions specified in the reference list from
+    # a list of recipes (each having a list of ingredients)
+    # reference list in stored in a .csv file (one row of strings)
+    reference_units = pd.read_csv(path_reference_units, squeeze=True)
+    return data.apply(remove_measure_units_single_recipe, reference_units=reference_units)
+
+
+def jaccard_coefficient(strings1: list, strings2: list):
+    s1 = set(strings1)
+    s2 = set(strings2)
+    return float(len(s1.intersection(s2)) / len(s1.union(s2)))
+
+
+def similarity(food1: str, food2: str):
+    # "tokenization" - takes lowercase strings and splits them into words
+    food1_tokens = list(filter(lambda a: a != ',', food1.lower().split()))
+    food2_tokens = list(filter(lambda a: a != ',', food2.lower().split()))
+    # implement method to compare similarity of food in recipe vs. food gotten by  REST API
+    # tags given as input should not have commas
+    # Jaccard-Coefficient: proportion of identical tokens
+    # Levensthein-Distance: number of coefficients to transform one sting into another
+    pass
+
+
+def request_database(food: str):
+    # method to request a certain food from the USDA food nutrition database
+    num_requests: int = 0
+    page_size: int = 1
+    query = {
+        'api_key': 'SoQlD3ha3vNDGM9ReqhdRBEj2j2EcYWaoIJipX5F',
+        'dataType': 'Foundation',
+        'foodTypes': 'Foundation',
+        'query': food,
+        'pageSize': page_size
+    }
+    # account for how many requests are made perhour (3600/hour max!)
+    # send a "dummy" query first to see if "Foundation" database has entries for that food
+    # if yes, take those. If no, search in "SR Legacy", then "Survey (FNDDS), then "Branded".
+    response = requests.get("https://api.nal.usda.gov/fdc/v1/foods/search", params=query)
+
+
+def get_food_category(food: str, categorized_foods: dict):
+    if food in categorized_foods:
+        return dict[food]
+    else:
+        # request database and determine food_category
+        pass
+    # return food category
+
+
+def add_categorized_food(food:str, category: str, categorized_foods: dict):
+    if food not in categorized_foods:
+        categorized_foods[food] = category
+    else:
+        print(food, ' already exists in dictionary.')
 
 # only load useful columns with df = pd.read_csv("filepath", usecols=list_useful_column_names)
 # specify data types to take less memory (e.g. for year-numbers use int.16 instead of int.64)
