2b2c3e9f81604589dcdc5e17c99eb04757a7ba6c
thomasbahne <thomas.bahne@gmx.net>
2021-05-16 16:33:22 +0200

    added new functions to functions.py, tests in rest_api_test.py

--- functions.py
+++ functions.py
@@ -1,4 +1,3 @@
-import Levenshtein as Levenshtein
 import matplotlib.pyplot
 import numpy as np
 import pandas as pd
@@ -86,28 +85,31 @@ def tokenize_fdc(phrase: str) -> list:
     tokens = [token.rstrip(' ') for token in tokens]
     tokens = [token.lstrip(' ') for token in tokens]
     tokens = [token for token in tokens if not token.isdigit()]
-    # to-do: remove 'measure unit' tokens
     return tokens
 
 
-def remove_measure_units_single_ingredient(ingredient: str, noise_terms: list):
+def remove_noise_terms_single_ingredient(ingredient: str, noise_terms: list, use_fdc_tokenizer=False) -> str:
     # removes terms that are not necessary to determine if ingredient is vegan/vegetarian (noise terms)
     # Example of noise terms: 'fresh', 'canned', 'frozen', 'tablespoon', 'sugar-free')
     # noise terms are specified in external .csv file
-    tokens: list = tokenize(ingredient)
+    if use_fdc_tokenizer:
+        tokenizer = tokenize_fdc
+    else:
+        tokenizer = tokenize
+    tokens: list = tokenizer(ingredient)
     tokens = [token for token in tokens if token not in set(noise_terms)]
-    return list(map(' '.join, tokens))
+    return ' '.join(tokens)
 
 
-def remove_measure_units_multiple_ingredients(ingredients: list, noise_terms: list):
+def remove_noise_terms_multiple_ingredients(ingredients: list, noise_terms: list):
     # for explanation, see function 'remove_measure_units_single_ingredient'
-    return [remove_measure_units_single_ingredient(ingredient=ingredient, noise_terms=noise_terms) for ingredient in
+    return [remove_noise_terms_single_ingredient(ingredient=ingredient, noise_terms=noise_terms) for ingredient in
             ingredients]
 
 
-def remove_measure_units(noise_terms: list, data: pd.core.series.Series):
+def remove_noise_terms(noise_terms: list, data: pd.core.series.Series):
     # for explanation, see function 'remove_measure_units_single_ingredient'
-    return [remove_measure_units_multiple_ingredients(ingredients=recipe, noise_terms=noise_terms) for recipe in data]
+    return [remove_noise_terms_multiple_ingredients(ingredients=recipe, noise_terms=noise_terms) for recipe in data]
 
 
 def jaccard_coefficient(tokens1: set, tokens2: set) -> float:
@@ -115,34 +117,39 @@ def jaccard_coefficient(tokens1: set, tokens2: set) -> float:
     return float(len(tokens1.intersection(tokens2)) / len(tokens1.union(tokens2)))
 
 
-def similarity(food_recipe: str, food_fdc: str) -> float:
-    # custom measure for similarity of two phrases (between 0 and 1)
-    # uses jaccard-coefficient as well as Levensthein distance
+def similarity(food_recipe: str, food_fdc: str, noise_terms: list) -> float:
+    # custom measure for similarity of two phrases (similarity is always a value between 0 and 1)
+    # uses Levensthein distance and devalues when number of tokens is unequal (inspired by Jaccard-Coefficient)
     # How it works:
-    # 1) take all tokens in ingredient from recipe
-    # 2) assign value 1 to tokens that also appear in the search result from the database
-    # 3) for remaining tokens, find most similar token (by Levensthein-Distance) in the set of tokens from the database
-    # 4) assign value exp(-0.2*Levensthein-Distance) to those tokens (factor of 0.2 chosen arbitrarily)
-    # 5) take average of values of all tokens in ingrdient from recipe and multiply by jaccard-coefficient
+    # 1) tokenize ingredients
+    # 2) take all tokens from ingredient in recipe and assign them a value:
+    # 2.a) assign value 1 to tokens that also appear in the search result from FDC (FoodData Central database)
+    # 2.b) assign exp(-0.2*max(Levensthein_Distance(token, tokens from search result)) to tokens not appearing in result
+    # from the database. Factor of 0.2 is chosen arbitrarily, I just needed a function that has value 1 at x=0 and
+    # is strictly decreasing, while asymptotically reaching 0
+    # 3) average the value of all tokens in ingredient
+    # 4) multiply average with exp(-0.1*|num_tokens_recipe - num_tokens_FDC|) to 'punish' difference in number of tokens
+
     recipe_tokens: list = tokenize(food_recipe)
-    fdc_tokens: list = tokenize_fdc(food_fdc)
+    fdc_tokens = remove_noise_terms_single_ingredient(ingredient=food_fdc, noise_terms=noise_terms,
+                                                      use_fdc_tokenizer=True).split()
     s1: set = set(recipe_tokens)
     s2: set = set(fdc_tokens)
-    jacc_coefficient: float = jaccard_coefficient(s1, s2)
     unique_s1: set = s1.difference(s2)
     unique_s2: set = s2.difference(s1)
-    if (jacc_coefficient == 1) or (len(unique_s1) == 0) or (len(unique_s2) == 0):
-        return 1
+    length_difference = abs(len(s1)-len(s2))
 
     # Levensthein-Distance: number of operations to transform one string into another
-    max_similarities = [min([Levenshtein.distance(token1, token2) for token2 in unique_s2]) for token1 in unique_s1]
+    min_levensthein_distances = np.array([min([Levenshtein.distance(token1, token2) for token2 in unique_s2])
+                                          for token1 in unique_s1])
     # to-do (optional): remove tokens from unique_s2 that are already used
     print('Recipe Tokens:', s1)
     print('FDC Tokens:', s2)
-    print('Jaccard Coefficient:', jacc_coefficient)
-    print('Max. similarities:', max_similarities)
-    max_similarities = np.exp(-0.2*max_similarities)
-    return jacc_coefficient*((len(s1.union(s2))+sum(max_similarities))/len(s1))
+    print('Lev. distances:', min_levensthein_distances)
+    min_levensthein_distances = np.exp(-0.2 * min_levensthein_distances)
+    print('Lev. distances:', min_levensthein_distances)
+    print('similarity:', np.exp(-0.1*length_difference) * ((len(s1.intersection(s2)) + sum(min_levensthein_distances)) / len(s1)))
+    return np.exp(-0.1*length_difference) * ((len(s1.intersection(s2)) + sum(min_levensthein_distances)) / len(s1))
 
 
 def request_database(food: str) -> Tuple[list, int]:
@@ -167,16 +174,16 @@ def request_database(food: str) -> Tuple[list, int]:
     num_hits_by_data_type: dict = response.json().get('aggregations').get('dataType')
 
     # take results of dataType which comes first in relevance and yields at least one results
-    for key in relevance:
-        if num_hits_by_data_type.get(key):
-            first_nonempty_data_type = key
+    for data_type in relevance:
+        if num_hits_by_data_type.get(data_type):
+            first_nonempty_data_type = data_type
             break
 
     if first_nonempty_data_type == '':
         return [None], 0
 
     # get up to five results from the most relevant dataType
-    page_size = max(num_hits_by_data_type.get(first_nonempty_data_type), 5)
+    page_size = min(num_hits_by_data_type.get(first_nonempty_data_type), 5)
     query.update({'dataType': first_nonempty_data_type,
                   'pageSize': page_size})
     response = requests.get("https://api.nal.usda.gov/fdc/v1/foods/search", params=query)
@@ -189,34 +196,35 @@ def request_database(food: str) -> Tuple[list, int]:
     return foods, num_requests_made
 
 
-def predict_food_category(foods: list) -> str:
-    # implement method that uses term similarity to determine best search result and take its food category
-    # if query matches 100% with result, take that result!!!
-    for food in foods:
-        updated_description = list(map(tokenize_fdc, tokenize_fdc(food.get('lowercaseDescription'))))
-    #    updated_description = # to-do: remove measure units
-        food.update({'lowercaseDescription': updated_description})
+def predict_food_category_of_request(food_recipe: str, fdc_foods: list, noise_terms: list) -> str:
+    # method that uses similarity to determine quality of search result and food category
     counters = {}
-    for food in foods:
-        counters[food.get('foodCategory')] = counters.get(food.get('foodCategory'), 0) + 1
+    for food in fdc_foods:
+        counters[food.get('foodCategory')] = counters.get(food.get('foodCategory'), 0) + similarity(
+            food_recipe=food_recipe, food_fdc=food.get('lowercaseDescription'), noise_terms=noise_terms)
 
     # when two categories have the same count, the category first added to counters is returned
     return max(counters, key=counters.get)
 
 
-def get_food_category(food: str, categorized_foods: dict) -> Tuple[str, int]:
+def get_food_category(food: str, categorized_foods: dict, noise_terms: list) -> Tuple[str, int]:
+    # searches in list of already categorized foods and returns category if found, otherwise send request to FDC
+    # database and predicts category given the search results from the database
     if food in categorized_foods:
         return categorized_foods.get(food), 0
     else:
         hits, num_requests = request_database(food)
         if hits[0] is None:
+            categorized_foods.update({food: 'Unidentified'})
             return 'Unidentified', num_requests
 
-        categorized_foods.update({food: predict_food_category(hits)})
-        return predict_food_category(hits), num_requests
+        predicted_category = predict_food_category_of_request(food_recipe=food, fdc_foods=hits, noise_terms=noise_terms)
+        categorized_foods.update({food: predicted_category})
+        return predicted_category, num_requests
 
 
-def categorize_foods(foods: list, categorized_foods: dict = None, queue_request_times: list = None) -> list:
+def categorize_multiple_ingredients(foods: list, noise_terms: list, categorized_foods: dict = None,
+                                    queue_request_times: list = None) -> list:
     if categorized_foods is None:
         categorized_foods: dict = {}
 
@@ -248,7 +256,7 @@ def categorize_foods(foods: list, categorized_foods: dict = None, queue_request_
         except StopIteration:
             break
 
-        category, num_requests = get_food_category(food, categorized_foods)
+        category, num_requests = get_food_category(food, categorized_foods, noise_terms=noise_terms)
         queue_request_times.append([num_requests, time.time()])
         cumulated_requests += num_requests
         categories.append(category)
@@ -256,8 +264,8 @@ def categorize_foods(foods: list, categorized_foods: dict = None, queue_request_
     return categories
 
 
-def categorize_recipes(preprocessed_ingredients: pd.core.series.Series, categorized_foods: dict = None,
-                       queue_request_times: list = None) -> pd.core.series.Series:
+def categorize_recipes(preprocessed_ingredients: pd.core.series.Series, noise_terms: list,
+                       categorized_foods: dict = None, queue_request_times: list = None) -> pd.core.series.Series:
     if categorized_foods is None:
         categorized_foods: dict = {}
     if queue_request_times is None:
@@ -265,14 +273,15 @@ def categorize_recipes(preprocessed_ingredients: pd.core.series.Series, categori
     recipe_categories: list = []
 
     for ingredients in preprocessed_ingredients:
-        recipe_categories.append(categorize_foods(foods=ingredients, categorized_foods=categorized_foods,
-                                                  queue_request_times=queue_request_times))
+        recipe_categories.append(categorize_multiple_ingredients(foods=ingredients, noise_terms=noise_terms,
+                                                                 categorized_foods=categorized_foods,
+                                                                 queue_request_times=queue_request_times))
 
     return pd.Series(recipe_categories)
 
 
 def batch_categorize_and_save(temp_category_save_path: str, batch_size: int, data_path: str,
-                              reference_units_path: str, categorized_foods_path: str = '', num_batches: int = 1,
+                              path_noise_term_file: str, categorized_foods_path: str = '', num_batches: int = 1,
                               skip_batches: int = 0):
     if os.path.isfile(categorized_foods_path):
         temp = pd.read_csv(categorized_foods_path, index_col=0)
@@ -281,15 +290,18 @@ def batch_categorize_and_save(temp_category_save_path: str, batch_size: int, dat
     else:
         categorized_foods: dict = {}
 
+    noise_terms = pd.read_csv(path_noise_term_file, squeeze=True).to_numpy().tolist()
+
     queue_request_times: list = []
 
     for batch_num in range(num_batches):
         data = pd.read_csv(data_path, header=0, usecols=['ingredients'], nrows=batch_size,
                            skiprows=range(1, 1 + (batch_size * batch_num) + skip_batches))
         data['ingredients'] = data['ingredients'].apply(literal_eval)
-        data['pp_ingredients'] = remove_measure_units(path_reference_units=reference_units_path,
-                                                      data=data['ingredients'])
-        data['categories'] = categorize_recipes(data['pp_ingredients'], categorized_foods, queue_request_times)
+        data['pp_ingredients'] = remove_noise_terms(noise_terms, data=data['ingredients'])
+        data['categories'] = categorize_recipes(preprocessed_ingredients=data['pp_ingredients'],
+                                                noise_terms=noise_terms, categorized_foods=categorized_foods,
+                                                queue_request_times=queue_request_times)
         temp2 = pd.DataFrame.from_dict(categorized_foods, orient="index")
         temp2.to_csv(categorized_foods_path)
         print(f'Batch {batch_num + 1}/{num_batches} processed and saved.')
@@ -297,7 +309,7 @@ def batch_categorize_and_save(temp_category_save_path: str, batch_size: int, dat
             data['categories'].to_csv(temp_category_save_path, mode='a', header=False)
         else:
             data['categories'].to_csv(temp_category_save_path, mode='a', header=['categories'])
-        # work-in-progress: save categories of foods in the original data file as a new column
+        # to-do: save categories of foods in the original data file as a new column
     pass
 
 # only load useful columns with df = pd.read_csv("filepath", usecols=list_useful_column_names)
